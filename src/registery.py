4439bd4f575506f7cd190af276e3c693b3aad2e0
Brandon Lord <bklord8878@gmail.com>
2025-02-03 23:53:10 -0600

    refactoring project structure

--- src/registery.py
+++ src/registery.py
@@ -1,12 +1,15 @@
+from concurrent.futures import ProcessPoolExecutor
+
 from .containers.containers import master_container
 
 
 def import_registery() -> None:
     container = master_container()
-
     imports = container.import_container()
 
     imports.preflight_script()
-    imports.bhcf_import()
-    imports.fi_import()
+
+    with ProcessPoolExecutor() as executer:
+        executer.map(lambda func: func(), [imports.bhcf_import, imports.fi_import])
+
     imports.transformation_script()
